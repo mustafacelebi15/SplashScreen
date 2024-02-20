7990978b98def05923868de96787f48b2b40f05c
mustafacelebi15 <96596130+mustafacelebi15@users.noreply.github.com>
2024-02-20 11:52:40 +0300

    SplashScreen

--- App.tsx
+++ App.tsx
@@ -5,9 +5,11 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import type {PropsWithChildren} from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import {
+  Platform,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -58,6 +60,9 @@ function Section({children, title}: SectionProps): React.JSX.Element {
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
